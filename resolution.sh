9e35987889ea15eef1dc331ad3ba91d9d7b56f64
ChillerDragon <ChillerDragon@gmail.com>
2021-05-12 18:56:38 +0200

    res script add skin file support

--- resolution.sh
+++ resolution.sh
@@ -5,6 +5,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
+is_skins=0
+skins_good_res=0
+skins_warning_res=0
+skins_error_res=0
+
+is_mapres=0
 good_res=0
 okay_res=0
 warning_res=0
@@ -22,6 +28,7 @@ then
     echo "  # path/to/images    type"
     echo "  foo/tiles           tiles"
     echo "  foo/quads           quads"
+    echo "  foo/skins 		skins"
     exit 0
 fi
 
@@ -33,80 +40,115 @@ then
 fi
 
 function check_dir() {
-    local dir=$1
-    local arg=$2
-    local w=0 e=0
-    local width=0 height=0
-    echo -n "[*] checking directory '$dir' ($arg) ... "
-    if [ ! -d "$dir" ]
-    then
-        echo ""
-        echo "Error: directory not found '$dir'"
-        echo "  is your current path and config correct?"
-        exit 1
-    fi
-    for img in "$dir"/*.png
-    do
-        [[ -e "$img" ]] || break
+	local dir=$1
+	local arg=$2
+	local w=0 e=0
+	local width=0 height=0
+	echo -n "[*] checking directory '$dir' ($arg) ... "
+	if [ ! -d "$dir" ]
+	then
+		echo ""
+		echo "Error: directory not found '$dir'"
+		echo "  is your current path and config correct?"
+		exit 1
+	fi
+	for img in "$dir"/*.png
+	do
+		[[ -e "$img" ]] || break
 
-        meta="$(file -b "$img")"
-        meta_type="${meta%% *}"
-        if [ ! "$meta_type" == "PNG" ]
-        then
-            echo ""
-            echo "Error: invalid file type expected 'PNG' got '$meta_type'"
-            echo "  $img"
-            exit 1
-        fi
-        if [ "$arg" == "quads" ]
-        then
-            continue
-        fi
-        meta_resolution="$(echo "$meta" | cut -d',' -f2 | cut -c 2-)"
-        if [ "$meta_resolution" == "1024 x 1024" ]
-        then
-            good_res="$((good_res + 1))"
-        elif [ "$meta_resolution" == "512 x 512" ]
-        then
-            okay_res="$((okay_res + 1))"
-            w=1
-        else
-            width="$(echo "$meta_resolution" | awk '{print $1}')"
-            height="$(echo "$meta_resolution" | awk '{print $3}')"
-
-            if [ "$width" -lt "512" ] || [ "$width" != "$height" ]
-            then
-                error_res="$((error_res + 1))"
-                error_files+="${RED}$meta_resolution${RESET} $img\\n"
-                e=1
-            else
-                warning_res="$((warning_res + 1))"
-            fi
-        fi
-    done
-    if [ "$e" == "1" ]
-    then
-        echo -e "${RED}ERROR"
-    elif [ "$w" == "1" ]
-    then
-        echo -e "${YELLOW}WARNING"
-    else
-        echo -e "${GREEN}OK"
-    fi
-    echo -en "${RESET}"
+		meta="$(file -b "$img")"
+		meta_type="${meta%% *}"
+		if [ ! "$meta_type" == "PNG" ]
+		then
+			echo ""
+			echo "Error: invalid file type expected 'PNG' got '$meta_type'"
+			echo "  $img"
+			exit 1
+		fi
+		meta_resolution="$(echo "$meta" | cut -d',' -f2 | cut -c 2-)"
+		if [ "$arg" == "quads" ]
+		then
+			is_mapres=1
+			continue
+		elif [ "$arg" == "skins" ]
+		then
+			is_skins=1
+			if [ "$meta_resolution" == "256 x 128" ]
+			then
+				skins_good_res="$((skins_good_res + 1))"
+			else
+				width="$(echo "$meta_resolution" | awk '{print $1}')"
+				height="$(echo "$meta_resolution" | awk '{print $3}')"
+				if [ "$width" -lt "256" ] || [ "$((width / 2 ))" != "$height" ]
+				then
+					skins_error_res="$((skins_error_res + 1))"
+					error_files+="${RED}$meta_resolution${RESET} $img\\n"
+					e=1
+				else
+					skins_warning_res="$((skins_warning_res + 1))"
+				fi
+			fi
+		elif [ "$arg" == "tiles" ]
+		then
+			is_mapres=1
+			if [ "$meta_resolution" == "1024 x 1024" ]
+			then
+				good_res="$((good_res + 1))"
+			elif [ "$meta_resolution" == "512 x 512" ]
+			then
+				okay_res="$((okay_res + 1))"
+				w=1
+			else
+				width="$(echo "$meta_resolution" | awk '{print $1}')"
+				height="$(echo "$meta_resolution" | awk '{print $3}')"
+				if [ "$width" -lt "512" ] || [ "$width" != "$height" ]
+				then
+					error_res="$((error_res + 1))"
+					error_files+="${RED}$meta_resolution${RESET} $img\\n"
+					e=1
+				else
+					warning_res="$((warning_res + 1))"
+				fi
+			fi
+		else
+			echo "Error: invalid type '$arg'"
+			exit 1
+		fi
+	done
+	if [ "$e" == "1" ]
+	then
+		echo -e "${RED}ERROR"
+	elif [ "$w" == "1" ]
+	then
+		echo -e "${YELLOW}WARNING"
+	else
+		echo -e "${GREEN}OK"
+	fi
+	echo -en "${RESET}"
 }
 
 function show_stats() {
-    echo -e "$error_files"
-    echo ""
-    echo "1024 x 1024: $good_res"
-    echo "512 x 512: $okay_res"
-    echo "warning: $warning_res"
-    echo "error: $error_res"
-    if [ "$error_res" -gt "0" ]
-    then
-        exit 1
-    fi
+	echo -e "$error_files"
+	echo ""
+	if [ "$is_mapres" == "1" ]
+	then
+		echo "[ mapres ]"
+		echo "1024 x 1024: $good_res"
+		echo "512 x 512: $okay_res"
+		echo "warning: $warning_res"
+		echo "error: $error_res"
+	fi
+	if [ "$is_skins" == "1" ]
+	then
+		echo "[ skins ]"
+		echo "256 x 128: $skins_good_res"
+		echo "warning: $skins_warning_res"
+		echo "error: $skins_error_res"
+	fi
+	if [ "$error_res" -gt "0" ]
+	then
+		exit 1
+	fi
 }
 
 while IFS= read -r line
